e367762abf9baf9dc6688e67f6b5b223dc79901a
Klaus Steigerwald <Klaus Steigerwald>
2016-11-17 11:31:50 +0100

    #23 Hinzufügen von einzelne Methoden in den service

--- src/core/synacta/api.service.ts
+++ src/core/synacta/api.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 // TODO shorten with systemjs
 import 'rxjs/add/operator/map'
+//import 'rxjs/add/operator/toPromise';
 
 const API_KEY = 'Token FHProjekt2016';
-const BASE_URL = 'https://synacta.agile-is.de/_api/base';
+const BASE_URL = 'https://synacta.agile-is.de/_api/base/';
 
 @Injectable()
 export class SynactaAPIService {
 
     baseHeaders: Headers = new Headers();
 
-    constructor(private http: Http) { 
+    constructor(private http: Http) {
         this.baseHeaders.append("Authorization", API_KEY);
     }
 
@@ -19,17 +20,66 @@ export class SynactaAPIService {
      *
      * returns an observable Json object
      */
-    public get(target: string, type: string, id:string) {
-        let endpoint = (target)? BASE_URL + target : BASE_URL;
-        let headers = new Headers(this.baseHeaders);
-        if (type) {
-            headers.append("type", type);
-        }
-        if (id) {
-            headers.append("id", id);
-        }
-        return this.http
-            .get(endpoint, {headers: headers})
-            .map(response => response.json());
-    }
+     public get(target: string, type: string, id:string) {
+         let endpoint = (target)? BASE_URL + target : BASE_URL;
+         let headers = new Headers(this.baseHeaders);
+         if (type) {
+             headers.append("type", type);
+         }
+         if (id) {
+             headers.append("id", id);
+         }
+         console.log(endpoint, target);
+         return this.http
+             .get(endpoint, {headers: headers})
+             .map(response => response.json());
+     }
+
+     public getRoot(type:string, id: string){
+       let url = BASE_URL + type + "/" + id;
+       return this.http
+           .get(url, {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
+
+     public getByID(type:string, id: string){
+       let url = BASE_URL + type + "/" + id;
+       return this.http
+           .get(url, {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
+     public getByType(type:string){
+       let url = BASE_URL + type;
+       return this.http
+          .get(url, {headers: this.baseHeaders})
+          .map(response => response.json());
+     }
+
+     public getChildren(type: string, id:string){
+       let url = BASE_URL + type + "/" + id + "/Children";
+       return this.http
+           .get(BASE_URL + '/{type}/{id}/Children', {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
+
+     public getChildTypes(type:string, id: string){
+       let url = BASE_URL + type + "/" + id +"/Children/types";
+       return this.http
+           .get(url, {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
+
+     public getDocuments(type:string, id: string){
+       let url = BASE_URL + type + "/" + id + "/Documents";
+       return this.http
+           .get(url, {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
+
+     public getDocTypes(type:string, id: string){
+       let url = BASE_URL + type + "/" + id + "/Documents/Types";
+       return this.http
+           .get(url, {headers: this.baseHeaders})
+           .map(response => response.json());
+     }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,23 +4,37 @@ import { NavController } from 'ionic-angular';
 
 import { SynactaAPIService } from '../../core/synacta/api.service';
 
+//import {Fubar} from './fubar';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  // will be changed asynchronously 
-  data;
+  // will be changed asynchronously
+  // just for example use
+  data00;
+  data01;
+  data02;
 
   constructor(public navCtrl: NavController, synAPI: SynactaAPIService) {
     // TODO proper error and completion handling without console.log
     // TODO parse the response object in the right way
-    synAPI.get("/root", null, null).subscribe(
-      response => this.data = response,
+    synAPI.get("root", null, null).subscribe(
+       response => this.data00 = response,
+       error => console.log(error),
+       () => console.log("Completed!", this.data00)
+    );
+    synAPI.getByID("Vorgang", "d154f762-66b5-41ec-a641-af950518e8fb").subscribe(
+      response => this.data01 = response,
       error => console.log(error),
-      () => console.log("Completed!")
+      () => console.log("Completed 2!", this.data01)
+    );
+    synAPI.getByType("Vorgang").subscribe(
+      response => this.data02 = response,
+      error => console.log(error),
+      () => console.log("Completed 2!", this.data02)
     );
   }
-
 }
