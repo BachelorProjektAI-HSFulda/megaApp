a5c8f0eef1bbae6e044be9c77b2a9a2215b47c70
Azmodis <christianziehe@web.de>
2016-11-24 19:58:32 +0100

    Fixes to #32
    fixed a lot of silly mistakes that came from inexperience with usage of
    TypeScript

--- src/core/synacta/objectCreator.ts
+++ src/core/synacta/objectCreator.ts
@@ -1,25 +1,25 @@
-﻿public createObject(response)
+﻿function createObject(response)
 {
     if (response.hasOwnProperty('Name')) {
-        dokument:Dokument = createDocument(response);
+        let dokument:Dokument = createDocument(response);
         return dokument;
     }
     else if (response.hasOwnProperty('Typ')) {
-        container:Container = createContainer(response);
+        let container:Container = createContainer(response);
         return container;
     }
 }
 
-public Dokument createDocument(response)
+function createDocument(response)
 {
-    dokument:Dokument = new Dokument(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
+    let dokument:Dokument = new Dokument(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
         response.IsVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
     return dokument;
 }
 
-public Container createContainer(response)
+function createContainer(response)
 {
-    container:Container = new Container(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
+    let container:Container = new Container(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
         response.isVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
     return container;
 }
