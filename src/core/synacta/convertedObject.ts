858f442899c3d17599b4619acb053b3eb06854c7
Azmodis <christianziehe@web.de>
2016-11-24 17:39:42 +0100

    #39 & #32 & #38
    1. coded "convertedObject.ts" which is used to model our object classes:
    - added abstract class "Entity"
    - added classes "Dokument" and "Container" which inherit from "Entity"
    2. coded "objectCreator.ts" which is used to create objects
    - added "createObject(response)" method which reads information from a
    JSON-object taken from "api.service.ts"'s request and calls a creation
    method after checking for the JSON-object's type
    - added "createDocument(response)" which is used to create an instance
    of type "Dokument"
    - added "createContainer(response)" which is used to create an instance
    of type "Container"
    ------------------------------
    TODO:
    1. fix "ObjectCreator.ts" since it basically appears to complain about
    everything as of now
    2. talk about how to connect responses from "api.service.ts" with
    services in this commit

---
+++ src/core/synacta/convertedObject.ts
@@ -0,0 +1,63 @@
+﻿abstract class Entity {
+    ID: string;
+    ObjectType: string;
+    Properties: Map<string,string>;
+    PropertyInfos: Map<string,string>;
+    ParentID: string;
+    ParentType: string;
+    IsVirtual: boolean;
+    Frozen: boolean;
+    CheckedOutBy: string;
+    Version: string;
+    Hash: string;
+    isFavorite: boolean;
+    //lastUsed: string; -> Wie soll Datum oder Zeit modelliert werden?
+
+    //@odata.context: string;
+    //@odata.readLink: string;
+    //Child@odata.navigationLink: string;
+    //Document@odata.navigationLink: string;
+    //Parent@odata.navigationLink: string;
+    //FullODataLink@odata.navigationLink: string;
+    //@odata.editLink: string;
+    //@odata.etag: string;
+    //@odata.metadataEtag: string;
+    //@odata.PicklistLink: string;
+
+    constructor(ID: string, ObjectType: string, Properties: Map<string, string>, PropertyInfos: Map<string, string>,
+        ParentID: string, ParentType: string, IsVirtual: boolean, Frozen: boolean, CheckedOutBy: string,
+        Version: string, Hash: string) {
+        this.ID = ID;
+        this.ObjectType = ObjectType;
+        this.Properties = Properties;
+        this.PropertyInfos = PropertyInfos;
+        this.ParentID = ParentID;
+        this.IsVirtual = IsVirtual;
+        this.Frozen = Frozen;
+        this.CheckedOutBy = CheckedOutBy;
+        this.Version = Version;
+        this.Hash = Hash;
+        this.isFavorite = false; //alle Objekte werden bei Erzeugung als Nicht-Favoriten intialisiert
+    }
+}
+
+    HasChild: boolean;
+
+    constructor(ID: string, ObjectType: string, Properties: Map<string, string>, PropertyInfos: Map<string, string>,
+        ParentID: string, ParentType: string, IsVirtual: boolean, Frozen: boolean, CheckedOutBy: string,
+        Version: string, Hash: string, HasChild:boolean) {
+        super(ID, ObjectType, Properties, PropertyInfos, ParentID, ParentType, IsVirtual, Frozen, CheckedOutBy, Version, Hash);
+        this.isFavorite = false;
+        this.HasChild = HasChild;
+    }
+}
+
+
+   
+    constructor(ID: string, ObjectType: string, Properties: Map<string, string>, PropertyInfos: Map<string, string>,
+        ParentID: string, ParentType: string, IsVirtual: boolean, Frozen: boolean, CheckedOutBy: string,
+        Version: string, Hash: string, HasChild: boolean) {
+        super(ID, ObjectType, Properties, PropertyInfos, ParentID, ParentType, IsVirtual, Frozen, CheckedOutBy, Version, Hash);
+        this.isFavorite = false;
+    }
+}
---
+++ src/core/synacta/objectCreator.ts
@@ -0,0 +1,19 @@
+﻿public createObject(response)
+{
+    if (response.hasOwnProperty('Name')) {
+        return dokument;
+    }
+    else if (response.hasOwnProperty('Typ')) {
+        return container;
+    }
+}
+
+public Dokument createDocument(response)
+{
+}
+
+public Container createContainer(response)
+{
+        response.isVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
+}
+
