2791f069e70c30e74d8fa633207c97f75ec9e94d
lionax <a.wellbrock@mailbox.org>
2016-11-28 20:40:29 +0100

    Approach no. 1 for #47

---
+++ src/core/synacta/api.objects.ts
@@ -0,0 +1,78 @@
+/**
+ * This object provides all functionality of the Synacta API
+ * All other objects are a subset of this Interface.
+ * TODO - find out which fields are optional
+ */
+export interface IGenericBaseEntity {
+    Properties?: any;
+    PropertyInfos?: any;
+    ID: string;
+    ObjectType: string;
+    ParentID: string;
+    ParentType: string;
+    IsVirtual: boolean;
+    Frozen: boolean;
+    HasChild: boolean;
+    CheckedOutBy: string;
+    Version: string;
+    Hash: string;
+}
+
+/**
+ * This object is either a container or a document
+ */
+export interface IEntity {
+    // Just for composition purposes
+}
+
+/**
+ * This object is a transmission frame
+ * with message and navigation metadata
+ */
+export interface IBaseEntityFrame<T> {
+    value: T[];
+}
+
+/**
+ * This object provides container specific properties and functions
+ */
+export interface IContainer extends IEntity {
+    Properties: Map<String, String>;
+    PropertyInfos?: Map<String, String>;
+    ID: string;
+    ObjectType: string;
+    ParentID: string;
+    ParentType: string;
+    IsVirtual: boolean;
+    Frozen: boolean;
+    HasChild: boolean;
+    Hash: string;
+    Children: Set<IEntity>;
+}
+
+/**
+ * This object provides document specific properties and methods
+ */
+export interface IDocument extends IEntity {
+    Properties: Map<String, String>;
+    PropertyInfos?: Map<String, String>;
+    ID: string;
+    ObjectType: string;
+    ParentID: string;
+    ParentType: string;
+    Frozen: boolean;
+    CheckedOutBy: string;
+    Version: string;
+    Hash: string;
+}
+
+/**
+ * This object contains organization specific properties
+ */
+ export interface IOrganization {
+    ID: string;
+    Name: string;
+    ParentID: string;
+    ParentName: string;
+    HasChildren: boolean;
+}
