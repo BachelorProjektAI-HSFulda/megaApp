a34518f25e34fbe39324bdc97f979e08cdd31c77
CreaDisc <adrian.kiesthardt@gmail.com>
2016-11-28 18:24:28 +0100

    commented out objectCreator.ts objectCreator.ts creates a TypeScript compilation error due to missing dependencies but is unused. Excluding the code from the project avoids the problem for now.

--- src/core/synacta/objectCreator.ts
+++ src/core/synacta/objectCreator.ts
@@ -1,26 +1,26 @@
-﻿function createObject(response)
-{
-    if (response.hasOwnProperty('Name')) {
-        let dokument:SynDocument = createDocument(response);
-        return dokument;
-    }
-    else if (response.hasOwnProperty('Typ')) {
-        let container:SynContainer = createContainer(response);
-        return container;
-    }
-}
+﻿// function createObject(response)
+// {
+//     if (response.hasOwnProperty('Name')) {
+//         let dokument:SynDocument = createDocument(response);
+//         return dokument;
+//     }
+//     else if (response.hasOwnProperty('Typ')) {
+//         let container:SynContainer = createContainer(response);
+//         return container;
+//     }
+// }
 
-function createDocument(response)
-{
-    let dokument:SynDocument = new Dokument(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
-        response.IsVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
-    return dokument;
-}
+// function createDocument(response)
+// {
+//     let dokument:SynDocument = new Dokument(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
+//         response.IsVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
+//     return dokument;
+// }
 
-function createContainer(response)
-{
-    let container:SynContainer = new Container(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
-        response.isVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
-    return container;
-}
+// function createContainer(response)
+// {
+//     let container:SynContainer = new Container(response.ID, response.ObjectType, response.Properties, response.PropertyInfos, response.ParentID, response.ParentType,
+//         response.isVirtual, response.Frozen, response.CheckedOutBy, response.Version, response.Hash, response.HasChild);
+//     return container;
+// }
 
