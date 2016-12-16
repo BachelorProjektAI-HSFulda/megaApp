b105fff2e5791b25a2a966e6b8b1f257af362fb2
Klaus Steigerwald <Klaus.Steigerwald@informatik.hs-fulda.de>
2016-12-16 01:50:18 +0100

    #35 implementing new module

--- src/pages/recent/recent.ts
+++ src/pages/recent/recent.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
@@ -6,19 +6,23 @@ import { SynactaAPIService } from '../../core/synacta/api.service';
 
 import { Favorits } from '../../core/storage/favorits';
 
+import { RecentList, ExpEntity} from '../../core/storage/recentList';
+
 @Component({
   selector: 'page-recent',
   templateUrl: 'recent.html'
 })
-export class RecentPage {
+export class RecentPage implements OnInit{
 
+  listOfRec
   // will be changed asynchronously
   // just for example use
   data00;
   data01;
   data02;
 
-  constructor(public navCtrl: NavController, synAPI: SynactaAPIService, private favList: Favorits) {
+  constructor(public navCtrl: NavController, synAPI: SynactaAPIService, private favList: Favorits,
+    private recList: RecentList) {
     favList.addTest("Vorgang", "32fae6ab-4ab1-48cc-8292-5fbf39258345");
     // TODO proper error and completion handling without console.log
     // TODO parse the response object in the right way
@@ -37,5 +41,15 @@ export class RecentPage {
       error => console.log(error),
       () => console.log("Completed 2!", this.data02)
     );
+    recList.addTest("Vorgang", "32fae6ab-4ab1-48cc-8292-5fbf39258345");
+    recList.addTest("Vorgang", "85f23fd7-14e4-4b5e-959f-a49f3137df8e");
+    recList.addTest("Vorgang", "8ee1ce7e-588c-4ece-b07c-b3fa2e1ec114");
+    favList.addTest("Vorgang", "d154f762-66b5-41ec-a641-af950518e8fb");
+    console.log(recList.getRec());
+    console.log(recList.loadEntitys());
+  }
+
+  ngOnInit():void{
+    this.listOfRec = this.recList.recList;
   }
 }
