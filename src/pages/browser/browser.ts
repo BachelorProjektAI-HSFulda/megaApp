563e3f1ae7b1fa0343125c550f00784c860d2500
fdai3973 <svenwernikowski@yahoo.de>
2016-12-16 14:37:58 +0100

    Add Favorit versuch 1 #69

--- src/pages/browser/browser.ts
+++ src/pages/browser/browser.ts
@@ -16,9 +16,7 @@ export class BrowserPage {
 	daten:Container;
 	kram;
   test;
-  constructor(public navCtrl: NavController, private favList: Favorits, private synAPI: SynactaAPIService) {
-    favList.addTest("Vorgang", "32fae6ab-4ab1-48cc-8292-5fbf39258345");
-    favList.addTest("Vorgang", "6b35df93-9e11-4796-b627-27e2abf0f3bd");
+  constructor(public navCtrl: NavController, private synAPI: SynactaAPIService) {
 
 
 	synAPI.getRoot().subscribe(
