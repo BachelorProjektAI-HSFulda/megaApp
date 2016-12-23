e228e9c528f929078bf073263f1e0e65173735c1
KlSteigerwald <Klaus.Steigerwald@informatik.hs-fulda.de>
2016-12-23 14:26:20 +0100

    Revert "#8 now you can add items from browser to your local favorites. Furthermore you can open an item from favorites in the browsertap"
    This reverts commit b936c62b174ff5bf7496dbbed7020702f3859716.

--- src/pages/browser/browser.ts
+++ src/pages/browser/browser.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 import { Favorits } from '../../core/storage/favorits';
 
 import { SynactaAPIService } from '../../core/synacta/api.service';
 
-import { Container, Entity } from '../../core/synacta/api.objects';
+import { Container } from '../../core/synacta/api.objects';
 
 @Component({
   selector: 'page-browser',
@@ -16,41 +16,29 @@ export class BrowserPage {
 	daten:Container;
 	kram;
   test;
-  constructor(public navCtrl: NavController, private synAPI: SynactaAPIService, private favList : Favorits, private navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, private synAPI: SynactaAPIService) {
 
+
+	synAPI.getRoot().subscribe(
+		response => this.daten = response,
+		error => console.log(error),
+		() => {
+        console.log("Root", this.daten);
+        synAPI.getChildren(this.daten).subscribe(
+          response => this.kram = response,
+          error => console.log(error),
+          () => console.log("Children", this.kram)
+
+        )
+
+    });
+
+  }
+  
   public deeper(children: Container): void{
 	this.synAPI.getChildren(children).subscribe(
 	response => this.kram = response,
 	error => console.log(error),
 	() => console.log("deeper", this.kram));
-  }
-
-  public addEntity(item : Container):void{
-    this.favList.addFav(item);
-  }
-
-  ionViewWillEnter(){
-    if(this.navParams.get('ID') == undefined){
-      this.synAPI.getRoot().subscribe(
-    		response => this.daten = response,
-    		error => console.log(error),
-    		() => {
-            console.log("Root", this.daten);
-            this.synAPI.getChildren(this.daten).subscribe(
-              response => this.kram = response,
-              error => console.log(error),
-              () => console.log("Children", this.kram)
-            )
-        });
-    }else{
-      let id = this.navParams.get('ID');
-      let type = this.navParams.get('ObjectType');
-      this.kram = new Array<Entity>();
-      this.synAPI.getByID(type, id).subscribe(
-        response => this.kram.push(response),
-        error => console.log(error),
-        () => console.log("reDirected" , this.navParams.get('ID'), this.kram));
-    }
   }
 }
