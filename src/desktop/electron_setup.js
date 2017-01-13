4576e0f2d6962178ddc9edeb9784e96d35680276
Azmodis <christianziehe@web.de>
2017-01-13 15:12:21 +0100

    #10 some more configurations
    Ionic doesn't give **** tho and overrides everything as of now

--- src/desktop/electron_setup.js
+++ src/desktop/electron_setup.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1366, height: 768})
+  mainWindow = new BrowserWindow({width: 1366, height: 768, center: true, title: 'MegaApp of Justice', titleBarStyle : 'hidden', defaultFontSize: 20})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
