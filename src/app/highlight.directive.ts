d4316ee9e26d3e6f218ef2e942644f3d9154880a
CreaDisc <adrian.kiesthardt@gmail.com>
2017-01-13 17:12:40 +0100

    enhanced swiping

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,18 +5,29 @@ import {Gesture} from 'ionic-angular/gestures/gesture';
 
 export class HighlightDirective implements OnInit, OnDestroy {
     el: HTMLElement;
+    button: HTMLElement;
   pressGesture: Gesture;
 
   constructor(el: ElementRef) {
     this.el = el.nativeElement;
+    this.button = document.getElementById("back_button");
+    console.log(this.button);
   }
 
   ngOnInit() {
     this.pressGesture = new Gesture(this.el);
     this.pressGesture.listen();
-    this.pressGesture.on('panright', e => {
-      console.log('pressed!!');
-    })
+    this.pressGesture.on('pan', e => {
+
+        var dir = e.direction;
+        var horizontal = (dir == 4 || dir == 2);
+
+        if(horizontal && e.deltaX >= 0) {
+            this.el.style.transform = "translateX("+e.deltaX+"px)";
+            this.button.style.transform = "translateX("+e.deltaX/2+"px)";
+        }
+
+    });
   }
 
   ngOnDestroy() {
