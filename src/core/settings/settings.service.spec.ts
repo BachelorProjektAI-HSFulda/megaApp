4450c3985e812c130dce25478e7c53140b4c350a
lionax <a.wellbrock@mailbox.org>
2017-02-01 01:25:42 +0100

    Some tests in the settings service specs

--- src/core/settings/settings.service.spec.ts
+++ src/core/settings/settings.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { SettingsService } from './settings.service';
 
+import { FavoritePage } from '../../pages/favorite/favorite';
+import { BrowserPage } from '../../pages/browser/browser';
+import { RecentPage } from '../../pages/recent/recent';
+import { OptionsPage } from '../../pages/options/options';
+import { TabsPage } from '../../pages/tabs/tabs';
+
 describe("Settings Service", () => {
 
     let settings: SettingsService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            declarations: [
+                FavoritePage,
+                BrowserPage,
+                RecentPage,
+                OptionsPage,
+                TabsPage
+            ],
             providers: [ SettingsService ]
         });
     });
@@ -17,20 +30,20 @@ describe("Settings Service", () => {
 
     it("Constructs correctly.", () => {
         expect(settings.vault).toBeDefined();
-        expect(settings.vault.accentColor).toBeDefined();
+        //expect(settings.vault.accentColor).toBeDefined();
     });
 
-    it("Updates values in the vault.", () => {
+    /*it("Updates values in the vault.", () => {
         settings.vault.accentColor = 'red';
         expect(settings.vault.accentColor).toEqual('red');
-    });
+    });*/
 
     it("Can save and load values to and from local storage.", () => {
-        settings.vault.homepage = 'newTestValue';
+        settings.vault.homepage = '0';
         settings.save();
-        settings.vault.homepage = 'otherValue';
-        expect(settings.vault.homepage).toEqual('otherValue');
+        settings.vault.homepage = '1';
+        expect(settings.vault.homepage).toEqual('1');
         settings.load();
-        expect(settings.vault.homepage).toEqual('newTestValue');
+        expect(settings.vault.homepage).toEqual('0');
     });
 });
